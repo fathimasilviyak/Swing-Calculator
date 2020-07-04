510e63bceedfc9f18772d185a2f398bf5e5d6f80
silviyaejas <silviyakvm@gmail.com>
2020-07-05 02:02:49 +0530

    creating window for calculator

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,4 +1,17 @@
+import javax.swing.JFrame;
 
 public class Calculator {
 
+	public Calculator() {
+		JFrame jf=new JFrame();
+		jf.setLayout(null);
+		jf.setSize(600, 600);  //width,height
+		jf.setVisible(true);   //
+		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//to stop the program execution once the window is closed
+	}
+   
+	public static void main(String[] args) {
+		new Calculator(); //object for class Calculator 
+	}
+	
 }
