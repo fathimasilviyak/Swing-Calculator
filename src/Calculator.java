46985b1447dde0732ac004dff51ab2e4b46a716e
silviyaejas <silviyakvm@gmail.com>
2020-07-15 01:24:01 +0530

    final swing calculator

--- src/Calculator.java
+++ src/Calculator.java
@@ -2,7 +2,6 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -220,6 +219,8 @@ public class Calculator implements ActionListener{
 		new Calculator();   //object for class Calculator 
 	}
 
+	
+	//Actions for buttons
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		
